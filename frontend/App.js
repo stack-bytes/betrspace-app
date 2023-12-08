af62ab8e962d0a6842fc160a34b846a87d2c7f26
David-Andrei Popa <59252699+davidphex@users.noreply.github.com>
2023-12-08 18:29:00 +0200

    added tailwind

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapScreen from './src/screens/HomeStack/MapScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapScreen />
       <StatusBar style="auto" />
     </View>
   );
---
+++ frontend/src/screens/HomeStack/MapScreen.jsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+export default function MapScreen(){
+    return (
+        <View className='flex justify-center items-center w-full h-full'>
+            <Text className='text-3xl font-bold'>
+                MAP SCREEN test
+            </Text>
+        </View>
+    )
+}
