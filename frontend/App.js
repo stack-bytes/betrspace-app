b18eaccd5b2aeae5738cdd5a4c9a56bb06af0b66
Andrei Dan <danandrei846@yahoo.com>
2023-12-09 01:11:05 +0200

    Delete unused assets and refactor code

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,6 @@
 import MainStack from './src/navigation/MainStack';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import ProfileScreen from './src/screens/ProfileStack/ProfileScreen';
 import {  useFonts, Nunito_300Light, Nunito_500Medium, Nunito_700Bold, Nunito_600SemiBold, Nunito_800ExtraBold, Nunito_900Black} from '@expo-google-fonts/nunito';
 const Root = createStackNavigator();
 
@@ -20,13 +19,13 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      {/*<Root.Navigator
+      <Root.Navigator
         id="Root"
         initialRouteName="MainStack"
       >
         <Root.Screen name="MainStack" component={MainStack} options={{headerShown: false}} />
-  </Root.Navigator>*/}
-    <ProfileScreen ></ProfileScreen>
+    </Root.Navigator>
+
     </NavigationContainer>
   );
 }
