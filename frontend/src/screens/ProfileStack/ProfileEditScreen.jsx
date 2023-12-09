f6f6dc3922477301cc72babb8b4a91d2f8e7341d
David-Andrei Popa <59252699+davidphex@users.noreply.github.com>
2023-12-09 13:23:01 +0200

    Refactor ProfileStack and ProfileEditScreen components

--- frontend/src/screens/ProfileStack/ProfileEditScreen.jsx
+++ frontend/src/screens/ProfileStack/ProfileEditScreen.jsx
@@ -1,14 +1,27 @@
-import {  ScrollView, TouchableOpacity} from "react-native-gesture-handler"
-import { View, Text, StyleSheet, Image } from "react-native"
+import { ScrollView } from "react-native-gesture-handler"
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native"
 import GenericInput from "../../components/Buttons/GenericInput"
 import {GenericButton} from "../../components/Buttons/GenericButton"
 import Separator from "../../components/Separator"
 
+import { useNavigation } from "@react-navigation/native"
+
 import  ImageSvg  from "../../../assets/icons/Image.svg"
+import ArrowIcon from "../../../assets/icons/arrow-icon.svg"
 
 const ProfileEditScreen = () => {
+    const navigation = useNavigation();
+
     return (
         <>
+        <View className='absolute top-16 right-4 h-12 w-12'>
+            <TouchableOpacity 
+                className = 'w-full h-full z-20'
+                onPress={() => console.log('test')}
+            >
+                <ArrowIcon />
+            </TouchableOpacity>
+        </View>
         <ScrollView className='w-full h-full p-5' contentContainerStyle={styles.flexMidScroll} >
 
             <View className="w-48 h-48 bg-grey mt-10 rounded-full flex flex-col items-center justify-center mb-1">
