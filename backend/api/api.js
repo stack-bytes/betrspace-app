1b295067e1b619b7e986781a9f4f41d4aa644dbf
Jarvx200 <jarvx404@gmail.com>
2023-12-09 12:15:14 +0200

    fixed api

--- backend/api/api.js
+++ backend/api/api.js
@@ -2,8 +2,10 @@ const { Router } = require('express');
 const router = Router();
 
 const locationApi = require('./routes/locationRoute');
+const userApi = require('./routes/userRoute')
 
 router.use('/location', locationApi);
+router.use('/users', userApi);
 
 
 module.exports = router;
