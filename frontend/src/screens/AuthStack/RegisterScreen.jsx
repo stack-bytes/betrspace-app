5865011ea563f0c698c98f7c828525d4182ea541
Jarvx200 <jarvx404@gmail.com>
2023-12-10 14:21:34 +0200

    added register page

--- frontend/src/screens/AuthStack/RegisterScreen.jsx
+++ frontend/src/screens/AuthStack/RegisterScreen.jsx
@@ -15,16 +15,16 @@ const RegisterScreen = () => {
         <View style={{ flex: 1, justifyContent: 'start', alignItems: 'center', padding: 0, }}>
             <Image source={appLogo} style={{ width: 250, height: 250 , marginTop: 60 }} />
             <Text className="text-6xl font-bold">Betr Space</Text>
-            <Text className="text-lg font-medium mt-8">Register to your account</Text>
+            <Text className="text-lg font-medium mt-8">Create your account</Text>
             <GenericInput placeholder={"Email"}></GenericInput>
             <GenericInput placeholder={"Password"}></GenericInput>
+            <GenericInput placeholder={"Re-enter Password"}></GenericInput>
             <GenericButton 
                 buttonText={"Register"} backgroundColor={"#FAFAFA"} borderColor={"#FAFAFA"} textColor={"#1A1A1A"}
                 width={200}
                 onPress={()=>navigation.navigate("MainStack")}
             />
-            <Text className="text-lg font-medium mt-8">- or login with -</Text>
-            <View className = 'flex-row items-center justify-center '>
+            <View className = 'flex-row items-center justify-center mt-5'>
             
             <View className="mr-3 ml-3 flex flex-row items-center justify-center">
                 <GenericButton 
@@ -45,10 +45,6 @@ const RegisterScreen = () => {
                 width={80} height={80}
             /></View>
             </View>
-
-            <Text className="text-lg font-medium mt-5 ">Don't have an account? <TouchableOpacity 
-            
-            ><Text className="font-bold text-primary text-lg">Sign up</Text></TouchableOpacity></Text>
         </View>
     );
 };
